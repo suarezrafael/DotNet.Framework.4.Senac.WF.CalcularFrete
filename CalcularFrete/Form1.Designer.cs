7eb556ab447d9a3b3f0820069f7bdf7db52c1beb
Rafael Vieira Suarez <rafaelv_s@hotmail.com>
2023-10-02 22:00:33 -0300

    alteracao tela frete

--- CalcularFrete/Form1.Designer.cs
+++ CalcularFrete/Form1.Designer.cs
@@ -28,33 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtUf = new System.Windows.Forms.TextBox();
+            this.txtFreteMinimo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalcularFrete = new System.Windows.Forms.Button();
+            this.txtTotalFrete = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // groupBox1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 112);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 571);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CALCULAR FRETE";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtTotalFrete);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(18, 310);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(557, 240);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "DADOS DO FRETE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Total Frete:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtUf);
+            this.groupBox2.Controls.Add(this.txtFreteMinimo);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnCalcularFrete);
+            this.groupBox2.Location = new System.Drawing.Point(18, 48);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(557, 240);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DADOS DO PACOTE";
+            // 
+            // txtUf
+            // 
+            this.txtUf.Location = new System.Drawing.Point(183, 95);
+            this.txtUf.Name = "txtUf";
+            this.txtUf.Size = new System.Drawing.Size(184, 34);
+            this.txtUf.TabIndex = 4;
+            // 
+            // txtFreteMinimo
+            // 
+            this.txtFreteMinimo.Location = new System.Drawing.Point(183, 49);
+            this.txtFreteMinimo.Name = "txtFreteMinimo";
+            this.txtFreteMinimo.Size = new System.Drawing.Size(184, 34);
+            this.txtFreteMinimo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UF:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Frete mínimo:";
+            // 
+            // btnCalcularFrete
+            // 
+            this.btnCalcularFrete.Location = new System.Drawing.Point(16, 173);
+            this.btnCalcularFrete.Name = "btnCalcularFrete";
+            this.btnCalcularFrete.Size = new System.Drawing.Size(276, 49);
+            this.btnCalcularFrete.TabIndex = 0;
+            this.btnCalcularFrete.Text = "CALCULAR FRETE";
+            this.btnCalcularFrete.UseVisualStyleBackColor = true;
+            this.btnCalcularFrete.Click += new System.EventHandler(this.btnCalcularFrete_Click);
+            // 
+            // txtTotalFrete
+            // 
+            this.txtTotalFrete.Location = new System.Drawing.Point(183, 65);
+            this.txtTotalFrete.Name = "txtTotalFrete";
+            this.txtTotalFrete.Size = new System.Drawing.Size(184, 34);
+            this.txtTotalFrete.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(653, 701);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(653, 617);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCalcularFrete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUf;
+        private System.Windows.Forms.TextBox txtFreteMinimo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTotalFrete;
     }
 }
 
--- CalcularFrete/Form1.cs
+++ CalcularFrete/Form1.cs
@@ -16,5 +16,37 @@ namespace CalcularFrete
         {
             InitializeComponent();
         }
+
+        private void btnCalcularFrete_Click(object sender, EventArgs e)
+        {
+            var freteMinimo = Convert.ToDecimal( txtFreteMinimo.Text );
+            var uf = txtUf.Text;
+
+            CalcularFrete(freteMinimo, uf);
+        }
+
+
+        void CalcularFrete(decimal freteMinimo, string uf)
+        {
+            // variavel do tipo decimal
+            var adicional = 5.0M;
+            var freteFinal = 0.0M;
+
+            // se uf == RS
+            if (uf.Equals("RS"))
+            {
+                freteFinal = freteMinimo + 0.5M;
+
+            }else if (uf == "SC" )
+            {
+                freteFinal = freteMinimo + 1.0M;
+            }
+            else
+            {
+                freteFinal = freteMinimo + adicional;
+            }
+
+            txtTotalFrete.Text = freteFinal.ToString("F2");
+        }
     }
 }
